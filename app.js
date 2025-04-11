cf7af9e6da3650d95201719549ec7978abb9f1b4
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-10 19:19:56 -0600

    html setup for table and base url tested

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("SBA-Data");
+const baseUrl = "https://swapi.dev/api/people/?page=1";
+
+async function getPeople() {
+  const data = await fetch(baseUrl)
+    .then((res) => res.json())
+    .then((character) => {
+      console.log(character);
+    });
+
+  return data;
+}
+
+getPeople();
