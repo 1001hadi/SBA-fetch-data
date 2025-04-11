c106326a26c9287e12382cc38f3d1af4afa21dea
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-10 19:45:27 -0600

    1st page of data fatched

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const tableRow = document.getElementById("table-row");
 const baseUrl = "https://swapi.dev/api/people/?page=1";
 
 async function getPeople() {
   const data = await fetch(baseUrl)
     .then((res) => res.json())
-    .then((character) => {
-      console.log(character);
+    .then((res) => {
+      console.log(res.results);
+    })
+    .catch((err) => {
+      console.log(err);
     });
 
-  return data;
 }
-
 getPeople();
