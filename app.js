56908ca269147127f9fa0aeda07fbe73ff7a3622
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-10 21:59:54 -0600

    previous btn added and logic coded but has bug

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const paginationEl = document.getElementById("pagination");
 
 const baseUrl = "https://swapi.dev/api/people/";
 
+// declare variables for current, next and previous pages
+let currPage = baseUrl;
+let nextPage;
+let prevPage;
+
+// add event listener to the dom content to load the fetched data when start
 document.addEventListener("DOMContentLoaded", handleFetchData);
 
 async function handleFetchData() {
+  // make sure clear existing data from table
+  tableBody.innerHTML = "";
+  // fetch main url and collect data
   await fetch(baseUrl)
     .then((response) => response.json())
     .then((data) => {
+      // assign next and previous vars to data
+      nextPage = data.next;
+      prevPage = data.previous;
       data.results.forEach((person) => {
         // Fetch homeworld and species data with Promise.all
         Promise.all([
@@ -29,6 +41,29 @@ async function handleFetchData() {
           tableBody.appendChild(tableRow);
         });
       });
+      // make sure handle the pagination after data loaded
     })
     .catch((err) => console.error("Error from API:", err));
+}
+
+// handle pagination function
+// make sure clear pagination story
+// check if there is previous page
+// create prev btn and assign name for it
+// make sure add event listener to previous btn to handle logic of fetching previous page
+// append btn to the pagination element
+// repeat the process fro next btn!!!
+
+function handlePagination() {
+  paginationEl.innerHTML = "";
+
+  if (prevPage) {
+    let prevBtn = document.createElement("button");
+    prevBtn.textContent = "PrevPage";
+    prevBtn.addEventListener("click", () => {
+      currPage = prevPage;
+      handleFetchData(prevPage);
+    });
+    paginationEl.appendChild(prevBtn);
+  }
 }
